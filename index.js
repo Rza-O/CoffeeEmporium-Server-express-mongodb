d8cac66074be4b0999efebf1f75ecc6dd1b159e2
appoclayptor <shahreza1414@gmail.com>
2024-11-28 21:05:17 +0600

    Stored mongo db user pass in env file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// DB_USER and DB_PASS
 
 app.use(cors());
 app.use(express.json());
